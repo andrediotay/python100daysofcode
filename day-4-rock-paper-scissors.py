35b0c23c901ac672b51f04c48479adb0ac76b240
Andre Diotay <andre.diotay@gmail.com>
2023-05-01 09:36:18 +0800

    Day 4 Activity: rock-paper-scissors game versus random computer

---
+++ day-4-rock-paper-scissors.py
@@ -0,0 +1,47 @@
+import random
+
+p_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.\n"))
+c_choice = random.randint(0,2)
+
+rock = """
+        _______
+    ---'   ____)
+          (_____)
+          (_____)
+          (____)
+    ---.__(___)
+    """
+paper = """
+         _______
+    ---'    ____)____
+               ______)
+              _______)
+             _______)
+    ---.__________)
+    """
+scissors = """
+        _______
+    ---'   ____)____
+              ______)
+           __________)
+          (____)
+    ---.__(___)
+    """
+win_map = [["draw","lose","win"],["win","draw","lose"],["lost","win","draw"]]
+
+if p_choice == 0:
+    print(rock)
+elif p_choice == 1:
+    print(paper)
+else:
+    print(scissors)
+
+print(f"Computer chose:")
+if c_choice == 0:
+    print(rock)
+elif c_choice == 1:
+    print(paper)
+else:
+    print(scissors)
+
+print(f"You {win_map[p_choice][c_choice]}.")
